e0033cc31ac715932b0677cd392de497ec9d3f3b
megarandolph <randolphmarte@hotmail.com>
2020-04-04 20:29:16 -0400

    Creacion de un layout estandar.

--- BancoNM/BancoNM/App_Start/BundleConfig.cs
+++ BancoNM/BancoNM/App_Start/BundleConfig.cs
@@ -24,7 +24,9 @@ namespace BancoNM
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/bootswatch.css",
+                      "~/Content/template.css"));
         }
     }
 }
