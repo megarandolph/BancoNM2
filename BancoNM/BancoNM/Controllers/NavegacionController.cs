ae94cdc4faae2cafc27b03fbdd60a6eb8aa89947
megarandolph <randolphmarte@hotmail.com>
2020-04-05 22:14:01 -0400

    Creacion de el controlador de navegacion.

---
+++ BancoNM/BancoNM/Controllers/NavegacionController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BancoNM.Controllers
+{
+    public class NavegacionController : Controller
+    {
+        // GET: Navegacion
+        public ActionResult Mantenimientos()
+        {
+            return View();
+        }
+        public ActionResult Consultas()
+        {
+            return View();
+        }
+        public ActionResult Solicitudes()
+        {
+            return View();
+        }
+        public ActionResult PagoPrestamo()
+        {
+            return View();
+        }
+    }
+}
